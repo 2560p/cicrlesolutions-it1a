c749bb839217b913d53e13004341cc1c4ead5d69
paul <112290055+2560p@users.noreply.github.com>
2022-10-03 16:49:17 +0200

    fix: deletion of index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-<title>Test page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-<h1>hello if u can see this, then it is working fine</h1>
+    <h1>hello this is a test</h1>
 </body>
-
 </html>
