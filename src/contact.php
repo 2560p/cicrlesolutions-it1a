966d91016fe4597d8def5fc086c8ded604af2329
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2022-10-11 09:58:47 +0200

    Started working on the contact form. Created grid container and the first input box

--- src/contact.php
+++ src/contact.php
@@ -1,3 +1,20 @@
 <? $pageTitle = "it works!";
 $cssFile = "index";
 include "../components/head_element.php"; ?>
+
+	<body>
+		<div class="gridContainer">
+			<? include "../components/header.php"; ?>
+
+			<div class="contactFormContainer">
+				<div class="contactForm">
+					<h1>Contact us</h1>
+
+					<input type="text" name="First Name" placeholder="First Name...">
+				</div>
+			</div>
+
+			<? include "../components/footer.php"; ?>
+		</div>
+	</body>
+	</html>
