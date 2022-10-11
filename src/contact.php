917fc888fdabb35d477e5e17bcf3dff9752f9763
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2022-10-11 10:50:02 +0200

    Created div for the contact information and started styling the content box

--- src/contact.php
+++ src/contact.php
@@ -1,17 +1,17 @@
 <? $pageTitle = "it works!";
-$cssFile = "index";
+$cssFile = "contact";
 include "../components/head_element.php"; ?>
 
 	<body>
 		<div class="gridContainer">
-			<? include "../components/header.php"; ?>
+<!--			<? include "../components/header.php"; ?> -->
 
 			<div class="contactFormContainer">
 				<div class="contactForm">
 					<h1>Contact us</h1>
 
 					<form name="contact" action="contact.php" method="GET">
-				
+
 						<input type="text" name="firstName" placeholder="First Name...">
 
 						<input type="text" name="lastName" placeholder="Last Name...">
@@ -26,9 +26,13 @@ include "../components/head_element.php"; ?>
 
 					</form>
 				</div>
+
+				<div class="contactInformation">
+					
+				</div>
 			</div>
 
-			<? include "../components/footer.php"; ?>
+<!--			<? include "../components/footer.php"; ?> -->
 		</div>
 	</body>
 	</html>
