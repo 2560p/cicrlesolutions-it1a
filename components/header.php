07278b018bcbac623567dc17efac75cb322dd383
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2022-10-11 12:02:12 +0200

    Add header element

--- components/header.php
+++ components/header.php
@@ -1,3 +1,9 @@
 <header>
-    <h1>hello world!</h1>
-</header>
+    <a id="firstHref" href="#"><img src="https://raw.githubusercontent.com/2560p/cicrlesolutions-it1a/main/src/img/circle%20solutions_logo_01.png" alt="circle solutions logo" width="265" height="87"></a>
+    <ul>
+      <li><a href="">Packages</a></li>
+      <li><a href="">FAQ</a></li>
+      <li><a href="">About us</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </header>
