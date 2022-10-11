652dd3b3245a10bcb0c08004ddd62e3d4ba292cc
timothyadewale <adewale.adewale@student.nhlstenden.com>
2022-10-11 06:01:08 -0400

    The package page was started, package images added and texts
    B
    
    A

---
+++ src/packages.php
@@ -0,0 +1,76 @@
+<? $pageTitle = "it works!";
+$cssFile = "index";
+include "../components/head_element.php"; ?>
+
+<body>
+<? include "../components/header.php"; ?>
+    <main>
+	<h1>Packages</h1>
+	<h2>Circle Workspace</h2>
+<img
+	src= "img/package_image1.jpeg"
+	alt= "package image 1"
+	width= "600px"
+	height= "480px"
+>
+	<h3>App management</h3>
+		<p>Create a workplace in minutes. We also want to make work easy for workplace administrators. We make it possible to create a role- and conditions-based workplace that combines all your current and future technologies within one workplace.</p>
+	<h3>Communication and information</h3>
+		<p>Display information from applications directly in Circle Workspace. At a glance, see all your unread emails, appointments, news, recently edited documents and more.</p>
+	<h3>Document management</h3>
+		<p>Bring all documents together in one online workplace. So you can work online and offline from any device. The documents app bundles and brings structure to Office 365, the fileserver, OneDrive and SharePoint.</p>
+	<h3>Pricing</h3>
+		<p>Pricing: 1-50 users €140,00 per month, 51-500 users €760,00 per month</p>
+	
+	<h2>Circle D.O.T.S.</h2>
+<img
+	src= "img/package_image2.jpeg"
+	alt= "package image2"
+	width= "600px"
+	height= "480px"
+>
+	<h3>Internal communications</h3>
+		<p>Keep everyone informed, no matter the change, things change – especially in the world today. Keeping everyone on the same page is a challenge for global businesses. Circle D.O.T.S. makes it easy to target and distribute news to the right people, analyse the impact and optimize the message. The result? Everyone gets where you’re going.</p>
+	<h3>Connect and engage</h3>
+		<p>Connect your colleagues and engage communities. If people don’t know each other, they can’t trust each other. If teams don’t know what they’re doing, they can’t help each other. Circle D.O.T.S. connects teams and people in an organic way – by letting them form Channels and Pages around projects, interests or social initiatives. No more unfamiliar faces, even if you’re fully remote.</p>
+	<h3>Knowledge management</h3>
+		<p>When people say “I love my job”, they’re not talking about the 7.6 hours per week spent trying to find information they need to do their work. Circle D.O.T.S. brings all your disparate resources, applications and knowledge together in one easily structured and searchable home.</p>
+	<h3>Analyze and optimise</h3>
+		<p>For a better working environment, what can be measured, can be improved. Circle D.O.T.S. helps you to shift from a “send and forget” to an “analyse and optimize” mindset, making it easier to experiment and evaluate in a data-driven way. The secret weapon? The world’s friendliest Analytics dashboard.</p>
+	<h3>Pricing</h3>
+		<p>1-50 users €180,00 per month, 51-500 users €799,00 per month</p>
+	
+	<h1>special package</h1>
+	
+<img
+	src= "img/encrypted_file.PNG"
+	alt= "encrypted_file image"
+	width= "600px"
+	height= "480px"
+>
+	<h3>Encrypted files</h3>
+<img
+	src= "img/map.jpg"
+	alt= "map image"
+	width= "600px"
+	height= "480px"
+>
+	<h3>Gps tracking for police</h3>
+<img
+	src= "img/instant_messaging.PNG"
+	alt= "instant messaging image"
+	width= "600px"
+	height= "480"
+>
+	<h3>Instant messaging</h3>
+	
+<a href="contact.php">Contact us for more information</a>
+
+	
+	
+    </main>
+
+<? include "../components/footer.php"; ?>
+
+</body>
+</html>
