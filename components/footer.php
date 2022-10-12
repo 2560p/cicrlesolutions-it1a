cb75353732f0b801af186b2b852e6fcabaa5da93
paul <112290055+2560p@users.noreply.github.com>
2022-10-12 16:48:49 +0200

    Fix: html validator, img src

--- components/footer.php
+++ components/footer.php
@@ -1,16 +1,16 @@
 <footer>
     <div>
-        <section><a href="https://www.instagram.com/"><img src="https://raw.githubusercontent.com/2560p/cicrlesolutions-it1a/main/src/img/Instagram.png" alt="instagram logo"></a></section>
-        <section><a href="https://www.twitter.com/"><img src="https://raw.githubusercontent.com/2560p/cicrlesolutions-it1a/main/src/img/Twitter.png" alt="twitter logo"></a></section>
-        <section><a href="https://www.facebook.com/"><img src="https://raw.githubusercontent.com/2560p/cicrlesolutions-it1a/main/src/img/Facebook.png" alt="facebook logo"></a></section>
+        <section><a href="https://www.instagram.com/"><img src="img/Instagram.png" alt="instagram logo"></a></section>
+        <section><a href="https://www.twitter.com/"><img src="img/Twitter.png" alt="twitter logo"></a></section>
+        <section><a href="https://www.facebook.com/"><img src="img/Facebook.png" alt="facebook logo"></a></section>
     </div>
     <div id="contacts">
         <p>+31 6 12 34 56 78</p>
-        <a href=""><img src="https://raw.githubusercontent.com/2560p/cicrlesolutions-it1a/main/src/img/Phone.png" alt="phone logo"></a>
+        <a href=""><img src="img/Phone.png" alt="phone logo"></a>
     </div>
     <div id="location">
         <p>Some adress Nl, Emmen</p>
-        <a href=""><img src="https://raw.githubusercontent.com/2560p/cicrlesolutions-it1a/main/src/img/Location.png" alt="phone logo"></a>
+        <a href=""><img src="img/Location.png" alt="phone logo"></a>
     </div>
 </footer>
 
--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 
-<head>
+<head lang="en">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/<?= $cssFile ?>.css">
