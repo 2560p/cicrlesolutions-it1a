4e80e40d106a05f075cc4cb5e6a7dac1eb325240
paul <112290055+2560p@users.noreply.github.com>
2022-10-12 16:16:49 +0200

    Fix: header in the main grid

--- components/header.php
+++ components/header.php
@@ -2,13 +2,14 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/global.css" />
-    <link rel="stylesheet" href="css/<?= $cssFile ?>.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/<?= $cssFile ?>.css">
     <title><?= $pageTitle ?></title>
 </head>
 
 <body>
+<div id="mainGrid">
 <header>
     <a id="firstHref" href="#"><img src="img/circle_solutions_logo_01.png" alt="circle solutions logo" width="265" height="87"></a>
     <ul>
@@ -18,5 +19,3 @@
         <li><a href="">Contact</a></li>
     </ul>
 </header>
-
-<div id="mainGrid">
