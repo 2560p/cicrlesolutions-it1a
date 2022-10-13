0cf5261681fd3ede7615067b068bef66b6809c88
Ada Bilici <105089757+AdaBilici@users.noreply.github.com>
2022-10-13 16:55:04 +0200

    Edit home page

--- src/index.php
+++ src/index.php
@@ -1,37 +1,59 @@
 <? $pageTitle = "it works!";
 $cssFile = "index";
 include "../components/header.php"; ?>
-    <main>
-    <div id="main-heading">
-                            <h1>circle solutions</h1>
-                            
-                        </div>
-                        <p>We connect the dots.</p>
-                        <p>we develop easy to use</p>
-                        <p><b>intranets</b> for all your employees</p>
-    <div id="packages">
-    <h2>Circle Workspace</h2>
-        <div class="box">
-            <b>App management</b>
-            <p>Manageable workplace in minutes</p>
-            <b>Communication and information</b>
-            <p>All information accsessible in the workspace</p>
-            <b>Document management</b>
-            <p>All your documents manageable from your workspace</p>
-        </div>
-    <p>More Information</p>
+<div id="mainheading">
+    <ul class="nobullet"> 
+        <li class="title">circle solutions</li>
+        <li class="subtitle">We connect the dots.</li>
+    </ul>    
+</div>
+<div>
+    <svg class="circles" viewBox="0 0 768 808" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M312.457 584.515C277.354 503.225 184.884 464.993 105.981 498.961C27.0783 532.929 -8.55712 626.311 26.5456 707.6C61.6483 788.89 154.118 827.122 233.021 793.154C311.924 759.186 347.559 665.804 312.457 584.515Z" stroke="#DCF1F9" stroke-width="4"/>
+        <path d="M752.246 259.212C717.143 177.923 624.673 139.69 545.77 173.658C466.867 207.626 431.232 301.008 466.335 382.298C501.437 463.587 593.907 501.82 672.81 467.852C751.713 433.884 787.349 340.501 752.246 259.212Z" stroke="#DCF1F9" stroke-width="4"/>
+        <path d="M300.329 100.522C265.223 19.2248 173.028 -19.1174 94.4771 14.699C15.9263 48.5154 -19.4403 141.773 15.6657 223.07C50.7717 304.367 142.966 342.709 221.517 308.893C300.068 275.076 335.435 181.819 300.329 100.522Z" stroke="#DCF1F9" stroke-width="4"/>
+        <line y1="-1.5" x2="176.287" y2="-1.5" transform="matrix(-0.0679914 -0.997686 -0.998859 0.0477553 165.771 493.169)" stroke="#DCF1F9" stroke-width="3"/>
+        <line y1="-1.5" x2="176.287" y2="-1.5" transform="matrix(-0.0679914 -0.997686 -0.998859 0.0477553 147.938 525.906)" stroke="#DCF1F9" stroke-width="3"/>
+        <line y1="-1.5" x2="175.882" y2="-1.5" transform="matrix(0.953533 0.301288 0.272611 -0.962124 299.74 199.914)" stroke="#DCF1F9" stroke-width="3"/>
+        <line y1="-1.5" x2="175.882" y2="-1.5" transform="matrix(0.953533 0.301288 0.272611 -0.962124 263.488 166.333)" stroke="#DCF1F9" stroke-width="3"/>
+        <line y1="-1.5" x2="242.551" y2="-1.5" transform="matrix(0.74532 -0.666706 -0.65405 -0.756452 310.741 586.019)" stroke="#DCF1F9" stroke-width="3"/>
+        <line y1="-1.5" x2="245.554" y2="-1.5" transform="matrix(0.74532 -0.666706 -0.65405 -0.756452 325.917 549.595)" stroke="#DCF1F9" stroke-width="3"/>
+    </svg>
+</div>
 
-    <h2>Circle D.O.T.S</h2>
-        <div class="box">
-            <b>Internal communications</b>
-            <p>Keep everyone informed no matter what</p>
-            <b>Connect and engage</b>
-            <p>Create channels and pages to improve connection</p>
-            <b>Knowledge management</b>
-            <p>All you need in one easy to use searchable home</p>
-            <b>And more...</b>
-        </div>
-    <p>More Information</p>
+
+<div id="secondheading">
+    <img src="img/Laptop.png" alt="Laptop with intranet">
+    <ul class="nobullet">
+        <li>we develop easy to use</li>
+        <li><b>intranets</b> for all your employees</li>
+    </ul>
+</div>
+
+
+<!-- <div id="packages">
+<h2>Circle Workspace</h2>
+    <div class="box">
+        <b>App management</b>
+        <p>Manageable workplace in minutes</p>
+        <b>Communication and information</b>
+        <p>All information accsessible in the workspace</p>
+        <b>Document management</b>
+        <p>All your documents manageable from your workspace</p>
     </div>
-    </main>
+<p>More Information</p>
+
+<h2>Circle D.O.T.S</h2>
+    <div class="box">
+        <b>Internal communications</b>
+        <p>Keep everyone informed no matter what</p>
+        <b>Connect and engage</b>
+        <p>Create channels and pages to improve connection</p>
+        <b>Knowledge management</b>
+        <p>All you need in one easy to use searchable home</p>
+        <b>And more...</b>
+    </div>
+<p>More Information</p>
+</div> -->
+
 <? include "../components/footer.php"; ?>
