2a6bf4012a43ebad55c324ad99f87b4a4c4dc9f0
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2022-10-17 18:03:12 +0200

    Added about us page first iteration

--- components/header.php
+++ components/header.php
@@ -11,7 +11,7 @@
 <body>
 <div id="mainGrid">
 <header>
-    <a id="firstHref" href="#"><img src="img/circle_solutions_logo_01.png" alt="circle solutions logo" width="265" height="87"></a>
+    <a id="firstHref" href="#"><img src="img/circle_solutions_logo_01.png" alt="circle solutions logo" height="80"></a>
     <ul>
         <li><a href="">Packages</a></li>
         <li><a href="">FAQ</a></li>
