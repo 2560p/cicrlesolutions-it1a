6bb5ad24f9c1e11608432242c5e5a869b814393a
timothyadewale <adewale.adewale@student.nhlstenden.com>
2022-10-23 10:06:01 -0400

    added changes to packages

--- src/packages.php
+++ src/packages.php
@@ -1,13 +1,144 @@
 <? $pageTitle = "Packages";
 $cssFile = "packages";
 include "../components/header.php"; ?>
+<div class="tophalfleft"> 
+	<div>
+		<h1 class="titletext">Packages</h1>
+		</div>
+	<div>
+		<h2 class="subtitleall">Circle Workspace</h2>
+		</div>
+			<div>
+				<img src="img/package_image1.png"
+					 alt="Pacahe package_image1"
+					 height="400px"
+					 width="400px" 
+					>
+				</div>
+					</div>
 
+				<div class="tophalfright">
+					<div>
+						<h3>App management</h3>	
+							<p>Create a workplace in minutes. We also want to make work easy for workplace administrators.We make it possible to create a 		role-and conditions-based workplace that combines all your current and future technologies within one workplace.  
+								</p>
+							</div>
+					<div>
+						<h3>Communication and information</h3>
+							<p> Display information from applications directly in Circle Workspace. 
+								At a glance, see all your unread emails, appointments, news, recently edited documents and more.
+								</p>
+					</div>
+
+<div> 
+	<h3>Document management</h3>
+		<p> Bring all documents together in one online workplace. So you can work online and offline from any device. The documents app bundles and brings structure to Office 365, the fileserver, OneDrive and SharePoint.
+		</p>
+			</div>
+<div>
+	<h3>Pricing</h3>
+		<p>Pricing: 1-50 users €140,00 per month, 51-500 users 	€760,00 per month </p>
+				</div>
+			</div>
+
+	<div class="middleright">
+		<div>
+			<h2 class="subtitleall">Circle D.O.T.S.</h2>
+				<img
+		 			src= "img/package_image2.png"
+		 			alt= "package image2"
+		 			width="400px"
+					height="400px"
+					>
+				</div>
+			</div>
+<div class="middleleft">
+		<div> 
+			<h3>Internal communications</h3>
+					<p class="classrighttext">Keep everyone informed, no matter the change, things change – especially in the world today. Keeping everyone on the same page is a challenge for global businesses. Circle D.O.T.S. makes it easy to target and distribute news to the right people, analyse the impact and optimize the message. The result? Everyone gets where you’re going.
+						</p>
+						 </div>
+		<div>				 
+			<h3>Connect and engage</h3>
+				<p>
+					Connect your colleagues and engage communities. If people don’t know each other, they can’t trust each other. If teams don’t know what they’re doing, they can’t help each other. Circle D.O.T.S. connects teams and people in an organic way – by letting them form Channels and Pages around projects, interests or social initiatives. No more unfamiliar faces, even if you’re fully remote.
+					</p>
+				</div>
+			<div>
+				<h3> Knowledge management</h3>
+					<p>
+						When people say “I love my job”, they’re not talking about the 7.6 hours per week spent trying to find information they need to do their work. Circle D.O.T.S. brings all your disparate resources, applications and knowledge together in one easily structured and searchable home.
+						</p>	
+			</div>
+
+			<div>
+				<h3>Analyze and optimise</h3>
+					<p>
+						For a better working environment, what can be measured, can be improved. Circle D.O.T.S. helps you to shift from a “send and forget” to an “analyse and optimize” mindset, making it easier to experiment and evaluate in a data-driven way. The secret weapon? The world’s friendliest Analytics dashboard.
+					</p>
+			</div>
+			<div>
+				<h3>Pricing</h3>
+					<p>
+						1-50 users €180,00 per month, 51-500 users €799,00 per month
+					</p>
+
+						</div>
+
+					</div>
+				
+		<div class="specialpack">
+			<h1 class="titletext">special package</h1>
+ 				</div>
+
+
+ <div class="spackpic">
+	<div>
+		<img src="img/encrypted_file.PNG"
+			 alt="encrypted_file"
+			 height="250px"
+			 width="250px"
+		>
+		<h3>Encrypted file</h3>
+	</div>
+
+	<div>
+			<img  src= "img/map.png" alt= "map image" width="300px" height="250px">
+		<h3>Gps tracking for police</h3>
+	</div>
+  <div>
+  	<img src= "img/instant_messaging.png" alt= "instant messaging image" width="250px" height="250px"
+  	>	
+  	<h3>Instant messaging</h3>
+  </div>
+ </div>
+
+ <br>
+<div class="contactlink">
+	<ul class="Contactuslist">
+			<li class="contactlinktext"><a href="contact.php" class="contactlinktext">Contact us</a></li>
+			<li class="contacttext">for more information </li>
+			
+			</ul>
+		</div>
+
+
+
+
+
+
+
+
+
+
+
+<!--
 <div class="flexpackage">
 	
 			<h1 class="classstitle">Packages</h1>
 		
 	</div>
-<body> <!-- Look up how to use the body tag, you shouldn't use it like that -->
+<body> 
 	<div class="maincontent">
 	<section>
 		<h2 class="classlefttitle"> Circle Workspace
@@ -106,7 +237,7 @@ include "../components/header.php"; ?>
 </section>
 </div>
 
-
+-->
 
 
 
