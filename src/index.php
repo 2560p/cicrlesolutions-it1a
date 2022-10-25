c6fe8a81c5a198282541bb91894498074ecd7890
Ada Bilici <105089757+AdaBilici@users.noreply.github.com>
2022-10-25 08:37:17 +0200

    Home page first half

--- src/index.php
+++ src/index.php
@@ -1,12 +1,14 @@
 <? $pageTitle = "it works!";
 $cssFile = "index";
 include "../components/header.php"; ?>
+
 <div id="mainheading">
     <ul class="nobullet"> 
         <li class="title">circle solutions</li>
         <li class="subtitle">We connect the dots.</li>
     </ul>    
 </div>
+
 <div>
     <svg class="circles" viewBox="0 0 768 808" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M312.457 584.515C277.354 503.225 184.884 464.993 105.981 498.961C27.0783 532.929 -8.55712 626.311 26.5456 707.6C61.6483 788.89 154.118 827.122 233.021 793.154C311.924 759.186 347.559 665.804 312.457 584.515Z" stroke="#DCF1F9" stroke-width="4"/>
@@ -30,30 +32,52 @@ include "../components/header.php"; ?>
     </ul>
 </div>
 
+<div id="workspace">
+    <h3>Circle Workspace</h3>
+			<div id="column" class="box1">
+				<b>App management</b>
+                <br></br>
+				<p>Manageable workplace in minutes</p>
+                <br></br>
+				<b>Communication and information</b>
+                <br></br>
+				<p>All information accsessible in the workspace</p>
+                <br></br>
+				<b>Document management</b>
+                <br></br>
+				<p>All your documents manageable from your workspace</p>
+			</div>
+            <div class="more">
+            <h4 style="color:#799DAB;">More infromation</h4>
+                <svg width="38" height="16" viewBox="0 0 38 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M37.7071 8.70711C38.0976 8.31658 38.0976 7.68342 37.7071 7.29289L31.3431 0.928932C30.9526 0.538408 30.3195 0.538408 29.9289 0.928932C29.5384 1.31946 29.5384 1.95262 29.9289 2.34315L35.5858 8L29.9289 13.6569C29.5384 14.0474 29.5384 14.6805 29.9289 15.0711C30.3195 15.4616 30.9526 15.4616 31.3431 15.0711L37.7071 8.70711ZM0 9L37 9V7L0 7L0 9Z" fill="#799DAB"/>
+</svg>
+            </div>
+</div>	
 
-<!-- <div id="packages">
-<h2>Circle Workspace</h2>
-    <div class="box">
-        <b>App management</b>
-        <p>Manageable workplace in minutes</p>
-        <b>Communication and information</b>
-        <p>All information accsessible in the workspace</p>
-        <b>Document management</b>
-        <p>All your documents manageable from your workspace</p>
-    </div>
-<p>More Information</p>
-
-<h2>Circle D.O.T.S</h2>
-    <div class="box">
-        <b>Internal communications</b>
-        <p>Keep everyone informed no matter what</p>
-        <b>Connect and engage</b>
-        <p>Create channels and pages to improve connection</p>
-        <b>Knowledge management</b>
-        <p>All you need in one easy to use searchable home</p>
-        <b>And more...</b>
-    </div>
-<p>More Information</p>
-</div> -->
+<div id="dots">
+    <h3>Circle D.O.T.S</h3>
+			<div id="column" class="box2">
+				<b>Internal communications</b>
+                <br></br>
+				<p>Keep everyone informed no matter what</p>
+                <br></br>
+				<b>Connect and engage</b>
+                <br></br>
+				<p>Create channels and pages to improve connection</p>
+                <br></br>
+				<b>Knowledge management</b>
+                <br></br>
+				<p>All you need in one easy to use searchable home</p>
+                <br></br>
+				<b style="color: #CF943C;">And more...</b>
+			</div>
+            <div class="more">
+                <h4 style="color:#799DAB;">More information</h4>
+                <svg width="38" height="16" viewBox="0 0 38 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M37.7071 8.70711C38.0976 8.31658 38.0976 7.68342 37.7071 7.29289L31.3431 0.928932C30.9526 0.538408 30.3195 0.538408 29.9289 0.928932C29.5384 1.31946 29.5384 1.95262 29.9289 2.34315L35.5858 8L29.9289 13.6569C29.5384 14.0474 29.5384 14.6805 29.9289 15.0711C30.3195 15.4616 30.9526 15.4616 31.3431 15.0711L37.7071 8.70711ZM0 9L37 9V7L0 7L0 9Z" fill="#799DAB"/>
+</svg>
+            </div>
+</div>
 
 <? include "../components/footer.php"; ?>
