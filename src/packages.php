1e57252a1f837325c2fa2857fa8fbe31e7d922cb
timothyadewale <adewale.adewale@student.nhlstenden.com>
2022-10-26 10:35:23 -0400

    responsive tweaking

--- src/packages.php
+++ src/packages.php
@@ -139,18 +139,18 @@ include "../components/header.php"; ?>
 	<div>
 		<img src="img/encrypted_file.PNG"
 			 alt="encrypted_file"
-			 height="250px"
+			 height="80%"
 			 width="250px"
 		>
 		<h3 class="endee">Encrypted file</h3>
 	</div>
 
 	<div class="police">
-			<img  src= "img/map.png" alt= "map image" width="300px" height="250px">
+			<img  src= "img/map.png" alt= "map image" width="250px" height="80%">
 		<h3 class="endee">Gps tracking for police</h3>
 	</div>
   <div>
-  	<img src= "img/instant_messaging.png" alt= "instant messaging image" width="250px" height="250px"
+  	<img src= "img/instant_messaging.png" alt= "instant messaging image" width="250px" height="80%"
   	>	
   	<h3 class="endee">Instant messaging</h3>
   </div>
@@ -175,176 +175,6 @@ include "../components/header.php"; ?>
 
 
 
-<!--
-<div class="flexpackage">
-	
-			<h1 class="classstitle">Packages</h1>
-		
-	</div>
-<body> 
-	<div class="maincontent">
-	<section>
-		<h2 class="classlefttitle"> Circle Workspace
-			</h2>
-				<img 
-					src="img/package_image1.png"
-					alt="package image"
-					width="500px"
-					height="550px"
-					>
 
-
-		
-		<h2 class="classlefttitle">Circle D.O.T.<Sw/h2>
-			<br>
-				<img
-		 			src= "img/package_image2.png"
-		 			alt= "package image2"
-		 			width="500px"
-					height="550px"
-					>
-		 	
-					</section>
-	<div class="maincontentleft">				
-	<section>
-		<h3 class="classrighttitle">App management</h3>
-
-			<p class="classrighttext">Create a workplace in minutes. We also want to make work easy for workplace administrators. We make it possible to create a role- and conditions-based workplace that combines all your current and future technologies within one workplace.
-			</p>
-		<h3 class="classrighttitle"> Communication and information</h3>
-
-			<p class="classrighttext">Display information from applications directly in Circle Workspace. 
-				At a glance, see all your unread emails, appointments, 
-				news, recently edited documents and more.
-				</p>
-		<h3 class="classrighttitle">Document management</h3>
-			<p class="classrighttext">Bring all documents together in one online workplace. 	So you can work online and offline from any device. The documents app 		bundles and brings structure to Office 365, the fileserver, OneDrive 	and SharePoint.
-				</p>
-		<h3 class="classrighttitle">Pricing</h3>
-			<p class="classrighttext">Pricing: 1-50 users €140,00 per month, 51-500 users 	€760,00 per month
-				</p>
-			
-
-	
-		
-		<h3 class="classrighttitle">Internal communications</h3>
-
-		<p class="classrighttext">Keep everyone informed, no matter the change, things change – especially in 			the world today. Keeping everyone on the same page is a challenge for 			global businesses. Circle D.O.T.S. makes it easy to target and 					distribute news to the right people, analyse the impact and 				optimize the message. The result? Everyone gets where you’re going.
-					</p>
-	<h3 class="classrighttitle">Connect and engage</h3>
-
-		<p class="classrighttext">Connect your colleagues and engage communities. If people don’t know each other, they can’t trust each other. If teams don’t know what they’re doing, they can’t help each other. Circle D.O.T.S. connects teams and people in an organic way – by letting them form Channels and Pages around projects, interests or social initiatives. No more unfamiliar faces, even if you’re fully remote.</p>
-
-	<h3 class="classrighttitle">Knowledge management</h3>
-		<p class="classrighttext">When people say “I love my job”, they’re not talking about the 7.6 hours 	per week spent trying to find information they need to do their work. 		Circle D.O.T.S. brings all your disparate resources, applications and 		knowledge together in one easily structured and searchable home.
-		</p>
-	<h3 class="classrighttitle">Analyze and optimise</h3>
-		<p class="classrighttext">For a better working environment, what can be measured, can be improved. 	Circle D.O.T.S. helps you to shift from a “send and forget” to an “analyse 		and optimize” mindset, making it easier to experiment and evaluate in a 		data-driven way. The secret weapon? The world’s friendliest Analytics 			dashboard.
-			</p>
-	<h3 class="classrighttitle">Pricing</h3>
-		<p class="classrighttext">1-50 users €180,00 per month, 51-500 users €799,00 per month
-		</p>
-
-	</section>
-</div>
-	</body>
-</div>
-
-
-
-<section>
-	<h1 class="classstitle">special package</h1>
-	
-	
-	<div class="flexbot">
-		
-		<div class="encryptimage"><img src= "img/encrypted_file.png" alt= "encrypted_file image" >
-					<h3 class="classrighttitle">Encrypted file</h3>
-					</div>
-						<div><img  src= "img/map.png" alt= "map image">
-						<h3 class="classrighttitle">Gps tracking for police</h3>
-						</div>
-						<div><img src= "img/instant_messaging.png" alt= "instant messaging image">	
-						<h3 class="classrighttitle">Instant messaging</h3>
-			</div>
-		</div>
-
-			
-	
-	<div class="classcontact">	
-	<a href="contact.php" class= "classrighttitle">Contact us for more information</a>
-</div>
-
-
-
-</section>
-</div>
-
--->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<!--<div id="flexTitle">
-		<ul>
-			<li><h1 class="classTitle1">Packages</h1></li>
-			<li><h2 class="classSubtitle">Circle Workspace</h2></li>
-		</ul>
-	</div>
-	<div id="flexImage1">
-		<img class="classImage1" src= "img/package_image1.png"	alt= "package image 1">
-	</div>
-	
-	
-		<h3 class="classSubtitle">App management</h3>
-		
-
-		
-			<p class="classBt1">Create a workplace in minutes. We also want to make work easy for workplace administrators. We make it possible to create a role- and conditions-based workplace that combines all your current and future technologies within one workplace.</p>
-	<h3 class="classSubtitle"> Communication and information</h3>
-
-		<p class="classBt1">Display information from applications directly in Circle Workspace. 
-		At a glance, see all your unread emails, appointments, 
-		news, recently edited documents and more.
-		</p>
-	<h3 class="classSubtitle">Document management</h3>
-		<p class="classBt1">Bring all documents together in one online workplace. So you can work online and offline from any device. The documents app bundles and brings structure to Office 365, the fileserver, OneDrive and SharePoint.</p>
-	<h3 class="classSubtitle">Pricing</h3>
-		<p class="classBt1">Pricing: 1-50 users €140,00 per month, 51-500 users €760,00 per month</p>
-	
-	<h2 class="classTitle1">Circle D.O.T.S.</h2>
-<img class="classImage2" src= "img/package_image2.png" alt= "package image2" >
-	<h3 class="classSubtitle">Internal communications</h3>
-		<p class="classBt1">Keep everyone informed, no matter the change, things change – especially in the world today. Keeping everyone on the same page is a challenge for global businesses. Circle D.O.T.S. makes it easy to target and distribute news to the right people, analyse the impact and optimize the message. The result? Everyone gets where you’re going.</p>
-	<h3 class="classSubtitle">Connect and engage</h3>
-
-		<p class="classBt1">Connect your colleagues and engage communities. If people don’t know each other, they can’t trust each other. If teams don’t know what they’re doing, they can’t help each other. Circle D.O.T.S. connects teams and people in an organic way – by letting them form Channels and Pages around projects, interests or social initiatives. No more unfamiliar faces, even if you’re fully remote.</p>
-	<h3 class="classSubtitle">Knowledge management</h3>
-		<p class="classBt1">When people say “I love my job”, they’re not talking about the 7.6 hours per week spent trying to find information they need to do their work. Circle D.O.T.S. brings all your disparate resources, applications and knowledge together in one easily structured and searchable home.</p>
-	<h3 class="classSubtitle">Analyze and optimise</h3>
-		<p class="classBt1">For a better working environment, what can be measured, can be improved. Circle D.O.T.S. helps you to shift from a “send and forget” to an “analyse and optimize” mindset, making it easier to experiment and evaluate in a data-driven way. The secret weapon? The world’s friendliest Analytics dashboard.</p>
-	<h3 class="classSubtitle">Pricing</h3>
-		<p class="classBt1">1-50 users €180,00 per month, 51-500 users €799,00 per month</p>
-	
-	<h1 class="classTitle">special package</h1>
-	
-	<div id="flexImages">
-		<img class="classEncrypted" src= "img/encrypted_file.png" alt= "encrypted_file image">
-		<img class="classMap" src= "img/map.png" alt= "map image">
-		<img class="classMessaging" src= "img/instant_messaging.png" alt= "instant messaging image">
-	</div>
-
-	<a href="contact.php">Contact us for more information</a>
--->
 
 <? include "../components/footer.php"; ?>
