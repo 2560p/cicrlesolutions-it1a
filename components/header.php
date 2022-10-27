85a7ddc2fb178bfd4779f213c20aee355325d115
paul <112290055+2560p@users.noreply.github.com>
2022-10-27 12:47:35 +0200

    fix: html lang attribute

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
-<head lang="en">
+<head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/<?= $cssFile ?>.css">
@@ -9,13 +9,13 @@
 </head>
 
 <body>
-<div id="mainGrid">
-<header>
-    <a id="firstHref" href="/"><img src="img/circle_solutions_logo_01.png" alt="circle solutions logo" height="80"></a>
-    <ul>
-        <li id="packages"><a href="packages">Packages</a></li>
-        <li id="faq"><a href="faq">FAQ</a></li>
-        <li id="about"><a href="about">About us</a></li>
-        <li id="contact"><a href="contact">Contact</a></li>
-    </ul>
-</header>
+    <div id="mainGrid">
+        <header>
+            <a id="firstHref" href="/"><img src="img/circle_solutions_logo_01.png" alt="circle solutions logo" height="80"></a>
+            <ul>
+                <li id="packages"><a href="packages">Packages</a></li>
+                <li id="faq"><a href="faq">FAQ</a></li>
+                <li id="about"><a href="about">About us</a></li>
+                <li id="contact"><a href="contact">Contact</a></li>
+            </ul>
+        </header>
