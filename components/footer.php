d780c79a6fd0b21a93917c23b5b1047ea68a4b1f
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2022-10-27 13:21:49 +0200

    Added traget=blank to footer

--- components/footer.php
+++ components/footer.php
@@ -1,16 +1,16 @@
 <footer>
     <div>
-        <section><a href="https://www.instagram.com/"><img src="img/Instagram.png" alt="instagram logo"></a></section>
-        <section><a href="https://www.twitter.com/"><img src="img/Twitter.png" alt="twitter logo"></a></section>
-        <section><a href="https://www.facebook.com/"><img src="img/Facebook.png" alt="facebook logo"></a></section>
+        <section><a href="https://www.instagram.com/" target="blank"><img src="img/Instagram.png" alt="instagram logo"></a></section>
+        <section><a href="https://www.twitter.com/" target="blank"><img src="img/Twitter.png" alt="twitter logo"></a></section>
+        <section><a href="https://www.facebook.com/" target="blank"><img src="img/Facebook.png" alt="facebook logo"></a></section>
     </div>
     <div id="contacts">
         <p>+31 6 12 34 56 78</p>
-        <a href=""><img src="img/Phone.png" alt="phone logo"></a>
+        <img src="img/Phone.png" alt="phone logo">
     </div>
     <div id="location">
         <p>Some adress Nl, Emmen</p>
-        <a href=""><img src="img/Location.png" alt="phone logo"></a>
+        <img src="img/Location.png" alt="phone logo">
     </div>
 </footer>
 
