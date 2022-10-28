9ebe70abf1a88c6c5873d2ea0b01d933c1c951c3
Nicanor Martinez <111961688+MartinezNicanor@users.noreply.github.com>
2022-10-28 14:48:43 +0200

    Fixed 4k scaling contact page

--- src/contact.php
+++ src/contact.php
@@ -73,6 +73,12 @@ if ($_POST) {
 						<line y1="1" x2="250" y2="1" stroke="#799DAB" stroke-width="2"/>
 						</svg>
 					</div>
+
+					<div class="separationLine4k">
+						<svg width="2" height="600" viewBox="0 0 2 594" fill="none"  xmlns="http://www.w3.org/2000/svg">
+						<line x1="1" y1="593.041" x2="1" y2="-0.000305176" stroke="#799DAB" stroke-width="2"/>
+						</svg>
+					</div>
 					
 
 					<div class="contactInformation">
